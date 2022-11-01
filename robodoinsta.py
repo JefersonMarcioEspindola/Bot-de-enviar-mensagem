28ddcffd496236e9e3e58a663ce5184c4c3a2523
Jeferson Márcio Espindola <jefersonmarciosteam@gmail.com>
2022-10-31 22:42:58 -0300

    robo 0.2

--- robodoinsta.py
+++ robodoinsta.py
@@ -1,11 +1,10 @@
 
 import pyautogui
-import pyperclip 
-import time 
+import pyperclip
+import time
 
 
-
-#Mensagens
+# Mensagens
 
 mns1 = f"""Opa tudo bom, me chamo Jeferson, e sou representante da Machine Tech."""
 mns2 = f"""Sou especialista em design e construção de sites de diversos nichos."""
@@ -16,16 +15,16 @@ mns6 = f"""O que você acha de conversarmos um pouco sobre seu negócio e princi
 mns7 = f"""Acredito que é uma área importantíssima e a maioria dos concorrentes não se atentam muito pra isso."""
 mns8 = f"""O que você me diz? eu tenho certeza que não vai ser perda de tempo para você."""
 
-#----------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------
 
-#Abrir o google
-
-time.sleep(1)
-pyautogui.hotkey("win", "2") 
+# Abrir o google
 
 time.sleep(1)
+pyautogui.hotkey("win", "2")
 
-#cliente 1 ------------------------------------------------------------------------------------------------------
+time.sleep(1)
+
+# cliente 1 ------------------------------------------------------------------------------------------------------
 
 pyautogui.hotkey("ctrl", "1")
 time.sleep(1)
@@ -36,56 +35,56 @@ for b in range(5):
     pyautogui.hotkey("shift", "tab")
     time.sleep(0.7)
 
-#mensagem 1
+# mensagem 1
 pyperclip.copy(mns1)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
-#mensagem 2
+# mensagem 2
 pyperclip.copy(mns2)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
-#mensagem 3
+# mensagem 3
 pyperclip.copy(mns3)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
-#mensagem 4
+# mensagem 4
 pyperclip.copy(mns4)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
-#mensagem 5
+# mensagem 5
 pyperclip.copy(mns5)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
-#mensagem 6
+# mensagem 6
 pyperclip.copy(mns6)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
-#mensagem 7
+# mensagem 7
 pyperclip.copy(mns7)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
-#mensagem 8
+# mensagem 8
 pyperclip.copy(mns8)
 pyautogui.hotkey("ctrl", "v")
 time.sleep(2)
@@ -93,9 +92,9 @@ pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
 
-#cliente 2 a 10 ------------------------------------------------------------------------------------------------
+# cliente 2 a 10 ------------------------------------------------------------------------------------------------
 
-for a in range(2): 
+for a in range(9):
 
     pyautogui.hotkey("ctrl", "tab")
     time.sleep(0.2)
@@ -106,56 +105,56 @@ for a in range(2):
         pyautogui.hotkey("shift", "tab")
         time.sleep(0.2)
 
-    #mensagem 1
+    # mensagem 1
     pyperclip.copy(mns1)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
     pyautogui.press("tab")
     pyautogui.press("enter")
     time.sleep(2)
-    #mensagem 2
+    # mensagem 2
     pyperclip.copy(mns2)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
     pyautogui.press("tab")
     pyautogui.press("enter")
     time.sleep(2)
-    #mensagem 3
+    # mensagem 3
     pyperclip.copy(mns3)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
     pyautogui.press("tab")
     pyautogui.press("enter")
     time.sleep(2)
-    #mensagem 4
+    # mensagem 4
     pyperclip.copy(mns4)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
     pyautogui.press("tab")
     pyautogui.press("enter")
     time.sleep(2)
-    #mensagem 5
+    # mensagem 5
     pyperclip.copy(mns5)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
     pyautogui.press("tab")
     pyautogui.press("enter")
     time.sleep(2)
-    #mensagem 6
+    # mensagem 6
     pyperclip.copy(mns6)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
     pyautogui.press("tab")
     pyautogui.press("enter")
     time.sleep(2)
-    #mensagem 7
+    # mensagem 7
     pyperclip.copy(mns7)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
     pyautogui.press("tab")
     pyautogui.press("enter")
     time.sleep(2)
-    #mensagem 8
+    # mensagem 8
     pyperclip.copy(mns8)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(2)
